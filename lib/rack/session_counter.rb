27c65906b324f24c371cb74405370f573412c129
Brian-Demon <BrianWMatejka@gmail.com>
2021-05-24 17:01:02 -0500

    solution 2

--- lib/rack/session_counter.rb
+++ lib/rack/session_counter.rb
@@ -9,6 +9,9 @@ module Rack
     def initialize(app, statsfile)
       @app = app
       @statsfile = statsfile
+      @rate_limit_window = 30
+      @rate_limit = 10
+      @cooldown_duration = 15
       load_stats
     end
 
@@ -29,11 +32,31 @@ module Rack
     end
 
     def log_call(env)
-      if env["rack.session"]["loginstate"]
-        login = Base64.decode64(env["rack.session"]["loginstate"])
+      if login = get_login_from_session(env)
         log_authenticated_call(login)
       else
         log_anonymous_call
+      end
+    end
+
+    def rate_limit_data(login:)
+      call_count_active = 0
+      last_call_time = nil
+      end_time_range = Time.now
+      start_time_range = end_time_range - @rate_limit_window
+      time_remaining = @rate_limit_window
+      if ::File.exist? @statsfile
+        CSV.foreach(@statsfile) do |(time, call_login)|
+          time = Time.at(time.to_i)
+          if call_login == login && time > start_time_range
+            call_count_active += 1
+            last_call_time = [time, last_call_time].compact.max
+            time_remaining = start_time_range - last_call_time
+          end
+        end
+        { active_count: call_count_active, last_call: last_call_time, time_remaining: time_remaining }
+      else
+        { active_count: 0, last_call: end_time_range - @rate_limit_window, time_remaining: time_remaining }
       end
     end
 
@@ -62,6 +85,26 @@ module Rack
       end
     end
 
+    def get_login_from_session(env)
+      if env["rack.session"]["loginstate"]
+        Base64.decode64(env["rack.session"]["loginstate"])
+      end
+    end
+
+    def rate_limit_exceeded?(env)
+      if login = get_login_from_session(env)
+        rate_limit_data = rate_limit_data(login: login)
+        if rate_limit_data[:active_count] > @rate_limit
+          time_offset = [rate_limit_data[:time_remaining], @cooldown_duration].max
+          if rate_limit_data[:last_call_time] + time_offset  > Time.now
+            return true
+          end
+        end
+      end
+
+      false
+    end
+
     def call(env)
       case env["PATH_INFO"]
       when "/_auth/stats"
@@ -71,8 +114,12 @@ module Rack
         headers = { "Content-Type" => "application/json" }
         [200, headers, [JSON.generate({ users: top_users(5) })]] 
       else
-        log_call(env)
-        @app.call(env)
+        if rate_limit_exceeded?(env)
+          [429, {}, ["Rate Limit Exceeded"]]
+        else
+          log_call(env)
+          @app.call(env)
+        end
       end
     end
   end
